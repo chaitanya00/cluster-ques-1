78cdac7591e52ecd35f89d382b9fe73fbd32dcef
chaitanya00 <chaitanyapawar0303@gmail.com>
2020-04-16 14:07:07 +0000

    updated sg.tf for pod communication with cluster api server

--- sg.tf
+++ sg.tf
@@ -14,7 +14,15 @@ resource "aws_security_group" "demo-cluster" {
     Name = "terraform-eks-demo"
   }
 }
-
+resource "aws_security_group_rule" "demo-cluster-ingress-node-https" {
+  description              = "Allow pods to communicate with the cluster API Server"
+  from_port                = 443
+  protocol                 = "tcp"
+  security_group_id        = "${aws_security_group.demo-cluster.id}"
+  source_security_group_id = "${aws_security_group.demo-node.id}"
+  to_port                  = 443
+  type                     = "ingress"
+}
 resource "aws_security_group_rule" "demo-cluster-ingress-workstation-https" {
   cidr_blocks       = ["0.0.0.0/0"]
   description       = "Allow to communicate with the cluster API Server"
--- wn-sg.tf
+++ wn-sg.tf
@@ -15,15 +15,7 @@ resource "aws_security_group" "demo-node" {
     "kubernetes.io/cluster/${var.cluster-name}" = "owned"
   }
 }
-resource "aws_security_group_rule" "demo-cluster-ingress-node-https" {
-  description              = "Allow pods to communicate with the cluster API Server"
-  from_port                = 443
-  protocol                 = "tcp"
-  security_group_id        = "${aws_security_group.demo-cluster.id}"
-  source_security_group_id = "${aws_security_group.demo-node.id}"
-  to_port                  = 443
-  type                     = "ingress"
-}
+
 resource "aws_security_group_rule" "demo-node-ingress-self" {
   description              = "Allow node to communicate with each other"
   from_port                = 0
